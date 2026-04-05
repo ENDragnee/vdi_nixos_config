1f1a1c0622e331b0ca147f17ac52cbe35b1e6db8
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-04-06 02:16:22 +0300

    feat(configuration.nix): tolerate cloud config error

--- configuration.nix
+++ configuration.nix
@@ -47,22 +47,22 @@
       RestartSec = "10s";
     };
   };
-  systemd.services.break-hostname-symlink = {
-    description = "Break NixOS hostname symlink for Cloud-Init";
-    before = [
-      "cloud-init.service"
-      "cloud-config.service"
-      "cloud-final.service"
-      "display-manager.service" # ← critical for first greeter
-      "systemd-logind.service"
-    ];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      ExecStart = "${pkgs.coreutils}/bin/rm -f /etc/hostname";
-    };
-  };
+  # systemd.services.break-hostname-symlink = {
+  #   description = "Break NixOS hostname symlink for Cloud-Init";
+  #   before = [
+  #     "cloud-init.service"
+  #     "cloud-config.service"
+  #     "cloud-final.service"
+  #     "display-manager.service" # ← critical for first greeter
+  #     "systemd-logind.service"
+  #   ];
+  #   wantedBy = ["multi-user.target"];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     RemainAfterExit = true;
+  #     ExecStart = "${pkgs.coreutils}/bin/rm -f /etc/hostname";
+  #   };
+  # };
 
   services.cloud-init = {
     enable = true;
@@ -103,8 +103,8 @@
   systemd.services.systemd-networkd-wait-online.enable = true;
   systemd.services.display-manager.after = [
     "systemd-user-sessions.service"
-    "cloud-init.service"
-    "cloud-config.service"
+    # "cloud-init.service"
+    # "cloud-config.service"
   ];
   # systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
   # systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
