5dc1a179afbc1121585f094e7eb0737dc5b55435
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2026-04-06 01:52:48 +0300

    feat(configuration.nix): error overrid

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,8 @@
     };
   };
   services.qemuGuest.enable = true;
+  systemd.services.cloud-config.serviceConfig.SuccessExitStatus = [0 1];
+  systemd.services.cloud-final.serviceConfig.SuccessExitStatus = [0 1];
   networking.networkmanager.enable = false;
   networking.useNetworkd = true;
   networking.useDHCP = false; # optional – uncomment if you want Proxmox cloud-init to fully control IP/DNS
